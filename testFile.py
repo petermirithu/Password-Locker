8ccbb89b670df29d287a36abfa0d6121a819cb5a
petermirithu <pyra_m.k@yahoo.com>
2019-11-08 20:36:00 +0300

    create a file for testing the function in userFile.py.

---
+++ testFile.py
@@ -0,0 +1,7 @@
+import unittest 
+from userFile import UserData
+
+class TestAccount(unittest.TestCase):
+  '''
+  Test class that checks if test cases for each function are correct and accurate.
+  '''
